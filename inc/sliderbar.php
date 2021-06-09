6195f922eeabef71739093f28ff8c9dbd1585ef6
vinhthuan14 <72190968+vinhthuan14@users.noreply.github.com>
2021-06-09 15:21:04 +0700

    Update sliderbar.php

--- inc/sliderbar.php
+++ inc/sliderbar.php
@@ -29,8 +29,93 @@
     <div class="row" style="    margin-right: -10px;margin-left: -10px;">
         <div class="xv-product-slides grid-view products" data-thumbnail="figure .xv-superimage" data-product=".xv-product-unit">
             <div class="row">
+                <h2>ĐIỆN THOẠI NỔI BẬT</h2>
                 <?php
-                $productNew = $product->getProductNew();
+                $productNew = $product->getProductNewByCatId(7);
+                if ($productNew) {
+                    while ($result_new = $productNew->fetch_assoc()) {
+                ?>
+                        <div class="col-md-3">
+                            <div class="xv-product-unit">
+                            <div class="xv-product ">
+                                <figure>
+                                    <a href="#"><img class="xv-superimage" src="admin/uploads/<?php echo $result_new["image"] ?>" alt=""  /></a>
+                                    <figcaption>
+                                        <ul class="style1" style="font-size: 13.4px;">
+                                            <li><a data-qv-tab="#qvt-wishlist" class="btn-cart flytoQuickView btn-square btn-blue" href="#"><i class="fa fa-heart"></i></a></li>
+                                            <li><a data-qv-tab="#qvt-compare" class="btn-cart flytoQuickView btn-square btn-blue" href="#"><i class="fa fa-exchange"></i></a></li>
+                                            <li><a class="btn-cart btn-square btn-blue" href="#"><i class="fa fa-expand"></i></a></li>
+                                        </ul>
+                                    </figcaption>
+                                </figure>
+                                <div class="xv-product-content">
+                                    <h3><a href="detail1.html"><?php echo $result_new["productName"] ?></a></h3>
+                                    <p>Aenean sollicitudin, nec sagittis sem lorem quist bibe dum auctor, nisi elit consequat ipsum. Duis sed odio sit amet nibh vulputate cursus a nec.</p>
+
+                                    <ul class="extra-links">
+                                        <li><a href="#"><i class="fa fa-heart"></i>Wishlist</a></li>
+                                        <li><a href="#"><i class="fa fa-exchange"></i>Compare</a></li>
+                                        <li><a href="#"><i class="fa fa-expand"></i>Expand</a></li>
+                                    </ul>
+                                    <!--ul-->
+                                    <div class="xv-rating stars-5"></div>
+                                    <span class="xv-price"><?php echo $fm->validation($result_new["price"] . " " . "VNĐ") ?></span>
+                                    <a data-qv-tab="#qvt-cart" href="#" class="product-buy flytoQuickView">Buy</a>
+                                </div>
+                                <!--xv-product-content-->
+                            </div>
+                            <!--xv-product(list-view)-->
+                        </div>
+                        </div>
+                <?php
+                    }
+                }
+                ?>
+                <h2>ĐỒNG HỒ NỔI BẬT</h2>
+                <?php
+                $productNew = $product->getProductNewByCatId(3);
+                if ($productNew) {
+                    while ($result_new = $productNew->fetch_assoc()) {
+                ?>
+                        <div class="col-md-3">
+                            <div class="xv-product-unit">
+                            <div class="xv-product ">
+                                <figure>
+                                    <a href="#"><img class="xv-superimage" src="admin/uploads/<?php echo $result_new["image"] ?>" alt=""  /></a>
+                                    <figcaption>
+                                        <ul class="style1" style="font-size: 13.4px;">
+                                            <li><a data-qv-tab="#qvt-wishlist" class="btn-cart flytoQuickView btn-square btn-blue" href="#"><i class="fa fa-heart"></i></a></li>
+                                            <li><a data-qv-tab="#qvt-compare" class="btn-cart flytoQuickView btn-square btn-blue" href="#"><i class="fa fa-exchange"></i></a></li>
+                                            <li><a class="btn-cart btn-square btn-blue" href="#"><i class="fa fa-expand"></i></a></li>
+                                        </ul>
+                                    </figcaption>
+                                </figure>
+                                <div class="xv-product-content">
+                                    <h3><a href="detail1.html"><?php echo $result_new["productName"] ?></a></h3>
+                                    <p>Aenean sollicitudin, nec sagittis sem lorem quist bibe dum auctor, nisi elit consequat ipsum. Duis sed odio sit amet nibh vulputate cursus a nec.</p>
+
+                                    <ul class="extra-links">
+                                        <li><a href="#"><i class="fa fa-heart"></i>Wishlist</a></li>
+                                        <li><a href="#"><i class="fa fa-exchange"></i>Compare</a></li>
+                                        <li><a href="#"><i class="fa fa-expand"></i>Expand</a></li>
+                                    </ul>
+                                    <!--ul-->
+                                    <div class="xv-rating stars-5"></div>
+                                    <span class="xv-price"><?php echo $fm->validation($result_new["price"] . " " . "VNĐ") ?></span>
+                                    <a data-qv-tab="#qvt-cart" href="#" class="product-buy flytoQuickView">Buy</a>
+                                </div>
+                                <!--xv-product-content-->
+                            </div>
+                            <!--xv-product(list-view)-->
+                        </div>
+                        </div>
+                <?php
+                    }
+                }
+                ?>
+                <h2>LAPTOP NỔI BẬT</h2>
+                <?php
+                $productNew = $product->getProductNewByCatId(2);
                 if ($productNew) {
                     while ($result_new = $productNew->fetch_assoc()) {
                 ?>
