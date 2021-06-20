0bbaf3565af50fff2f9bae78b7d00e931902a035
DinhNPVy <4501104282@student.hcmue.edu.vn>
2021-06-20 21:23:52 +0700

    upload slider

--- inc/slider_1.php
+++ inc/slider_1.php
@@ -1,156 +1,215 @@
-
 <style>
-* {box-sizing: border-box}
-body {font-family: Verdana, sans-serif; margin:0}
-.mySlides {display: none}
-img {vertical-align: middle;}
+  * {
+    box-sizing: border-box
+  }
 
-/* Slideshow container */
-.slideshow-container {
-  max-width: 60%;
-  position: relative;
-  margin: 10px 0px;
-}
+  body {
+    font-family: Verdana, sans-serif;
+    margin: 0
+  }
 
-/* Next & previous buttons */
-.prev, .next {
-  cursor: pointer;
-  position: absolute;
-  top: 50%;
-  width: auto;
-  padding: 16px;
-  margin-top: -22px;
-  color: white;
-  font-weight: bold;
-  font-size: 18px;
-  transition: 0.6s ease;
-  border-radius: 0 3px 3px 0;
-  user-select: none;
-}
+  .mySlides {
+    display: none
+  }
 
-/* Position the "next button" to the right */
-.next {
-  right: 0;
-  border-radius: 3px 0 0 3px;
-}
+  img {
+    vertical-align: middle;
+  }
 
-/* On hover, add a black background color with a little bit see-through */
-.prev:hover, .next:hover {
-  background-color: rgba(0,0,0,0.8);
-}
+  /* Slideshow container */
+  .slideshow-container {
+    max-width: 70%;
+    position: relative;
+    margin: 10px 0px;
+  }
 
-/* Caption text */
-.text {
-  color: #f2f2f2;
-  font-size: 15px;
-  padding: 8px 12px;
-  position: absolute;
-  bottom: 8px;
-  width: 100%;
-  text-align: center;
-}
+  /* Next & previous buttons */
+  .prev,
+  .next {
+    cursor: pointer;
+    position: absolute;
+    top: 50%;
+    width: auto;
+    padding: 16px;
+    margin-top: -42px;
+    color: white;
+    font-weight: bold;
+    font-size: 18px;
+    transition: 0.6s ease;
+    border-radius: 0 3px 3px 0;
+    user-select: none;
+  }
 
-/* Number text (1/3 etc) */
-.numbertext {
-  color: #f2f2f2;
-  font-size: 12px;
-  padding: 8px 12px;
-  position: absolute;
-  top: 0;
-}
+  /* Position the "next button" to the right */
+  .next {
+    right: 0;
+    border-radius: 3px 0 0 3px;
+  }
 
-/* The dots/bullets/indicators */
-.dot {
-  cursor: pointer;
-  height: 15px;
-  width: 15px;
-  margin: 0 2px;
-  background-color: #bbb;
-  border-radius: 50%;
-  display: inline-block;
-  transition: background-color 0.6s ease;
-}
+  /* On hover, add a black background color with a little bit see-through */
+  .prev:hover,
+  .next:hover {
+    background-color: rgba(0, 0, 0, 0.8);
+  }
 
-.active, .dot:hover {
-  background-color: #717171;
-}
+  /* Caption text */
+  .text {
+    color: #f2f2f2;
+    font-size: 15px;
+    padding: 8px 12px;
+    position: absolute;
+    bottom: 8px;
+    width: 100%;
+    text-align: center;
+  }
 
-/* Fading animation */
-.fade {
-  -webkit-animation-name: fade;
-  -webkit-animation-duration: 3s;
-  animation-name: fade;
-  animation-duration: 3s;
-}
+  /* Number text (1/3 etc) */
+  .numbertext {
+    color: #f2f2f2;
+    font-size: 12px;
+    padding: 8px 12px;
+    position: absolute;
+    top: 0;
+  }
 
-@-webkit-keyframes fade {
-  from {opacity: .4}
-  to {opacity: 1}
-}
+  /* The dots/bullets/indicators */
+  .dot {
+    cursor: pointer;
+    height: 15px;
+    width: 15px;
+    margin: 0 2px;
+    background-color: #bbb;
+    border-radius: 50%;
+    display: inline-block;
+    transition: background-color 0.6s ease;
+  }
 
-@keyframes fade {
-  from {opacity: .4}
-  to {opacity: 1}
-}
+  .active,
+  .dot:hover {
+    background-color: #717171;
+  }
 
-/* On smaller screens, decrease text size */
-@media only screen and (max-width: 300px) {
-  .prev, .next,.text {font-size: 11px}
-}
+  /* Fading animation */
+  .fade {
+    -webkit-animation-name: fade;
+    -webkit-animation-duration: 3s;
+    animation-name: fade;
+    animation-duration: 3s;
+  }
+
+  @-webkit-keyframes fade {
+    from {
+      opacity: .4
+    }
+
+    to {
+      opacity: 1
+    }
+  }
+
+  @keyframes fade {
+    from {
+      opacity: .4
+    }
+
+    to {
+      opacity: 1
+    }
+  }
+
+  /* On smaller screens, decrease text size */
+  @media only screen and (max-width: 300px) {
+
+    .prev,
+    .next,
+    .text {
+      font-size: 11px
+    }
+  }
+
+  .preorder-hot {
+    display: flex;
+    flex-wrap: wrap;
+    margin-left: auto;
+    padding-top: 9px;
+    width: 275px
+  }
+
+  .preorder-hot a {
+    margin-bottom: 5px;
+    padding-left: 10px;
+    width: 50%
+  }
+
+  .preorder-hot a img {
+    width: 100%;
+    border-radius: 4px
+  }
 </style>
 <div class="container">
-    <div class="slideshow-container">
- <?php
-                $getSlider = $product->show_slider();
-                if ($getSlider) {
-                    while ($result = $getSlider->fetch_assoc()) {
-                ?>
-                        <div class="mySlides ">
-                              <img src="admin/uploads/<?php echo $result["slider_image"] ?>" style="width:100%">
-                        </div>
-                <?php
-                    }
-                }
-                ?>
+  <div class="slideshow-container">
+    <?php
+    $getSlider = $product->show_slider();
+    if ($getSlider) {
+      while ($result = $getSlider->fetch_assoc()) {
+    ?>
+        <div class="mySlides ">
+          <img src="admin/uploads/<?php echo $result["slider_image"] ?>" style="width:100%">
+        </div>
+    <?php
+      }
+    }
+    ?>
 
-<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-<a class="next" onclick="plusSlides(1)">&#10095;</a>
-<br>
+    <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+    <a class="next" onclick="plusSlides(1)">&#10095;</a>
+    <br>
+  </div>
+  <div class="preorder-hot">
+    <a aria-label="slide" data-cate="0" data-place="1548" href=""><img src="img/bannerdt-340x340-2.jpg" alt="iphone"></a>
+    <a aria-label="slide" data-cate="0" data-place="1548" href=""><img src="img/laptopdesk-340x340.jpg" alt="Galaxy S21"></a>
+    <a aria-label="slide" data-cate="0" data-place="1548" href=""><img src="img/diệnthoaidesk-340x340-2.jpg" alt="Laptop Gaming"></a>
+    <a aria-label="slide" data-cate="0" data-place="1548" href=""><img src="img/─æie╠ú╠énthoaidesk-340x340.jpg" alt="Airtag"></a>
+  </div>
 
-</div>
+
 
 
 </div>
 
 <script>
+  var slideIndex = 1;
+  showSlides(slideIndex);
 
-var slideIndex = 1;
-showSlides(slideIndex);
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+      slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
+    }
+    for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
 </script>
 
 </body>
+
 </html>
